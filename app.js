0ae41630b3448154263bb3275743096be5cc7d5f
manuel-heav <manuelheav32@gmail.com>
2023-07-20 10:59:55 +0300

    bugs

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
+
+const methodOverride = require("method-override");
 const connectDB = require("./server/config/db");
 const session = require("express-session");
 const passport = require("passport");
@@ -26,6 +28,7 @@ app.use(passport.session());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(methodOverride("_method"));
 app.use(express.static("public"));
 
 app.use(expressLayouts);
